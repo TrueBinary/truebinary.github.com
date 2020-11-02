25c0fc4ae083f31c4ab9c13c9715fca5d1699270
TrueBinary <gui15787@gmail.com>
2020-11-02 19:52:48 -0300

    continuando testendo

---
+++ script.js
@@ -0,0 +1,27 @@
+// Set the date we're counting down to
+const countDownDate = new Date("Nov 27, 2020").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s `;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
